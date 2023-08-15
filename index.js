2852e162f496c85d2efbcdf7aee538dbdd952d14
yiconic <rp5865442@gmail.com>
2023-08-16 01:43:29 +0530

    fix cros polyce

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-import express, { urlencoded } from 'express';
-import path from 'path';
+import express, { urlencoded } from "express";
+import path from "path";
 
-import cookieParser from 'cookie-parser';
-import * as userRouter from './routs/user.js'
-import * as taskRouter from './routs/task.js'
-export   const app=express();
-import {config} from 'dotenv';
-import * as middelware from './middelewares/auth.js'
-import {errormiddelware} from './middelewares/error.js'
-import cors from 'cors';
-config()
+import cookieParser from "cookie-parser";
+import * as userRouter from "./routs/user.js";
+import * as taskRouter from "./routs/task.js";
+export const app = express();
+import { config } from "dotenv";
+import * as middelware from "./middelewares/auth.js";
+import { errormiddelware } from "./middelewares/error.js";
+import cors from "cors";
+import { get } from "http";
+config();
 
-app.set("view engine","ejs")
-app.use(cors())
-app.use(express.static(path.join(path.resolve(),"public")));
+app.set("view engine", "ejs");
+app.use(cors(
+    {
+         credentials: true,
+          origin: [process.env.FRONTEND_URL]
+         }));
+app.use(express.static(path.join(path.resolve(), "public")));
 
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -26,19 +30,12 @@ app.use(express.json());
 //     credentials:true
 // }))
 
+app.use("/api/v1/user", userRouter.router);
+app.use("/api/v1/task", taskRouter.router);
 
-app.use("/api/v1/user",userRouter.router);
-app.use("/api/v1/task",taskRouter.router);
+app.get("/", middelware.isautancate, (req, res) => {
+  //users home page
+  res.send(`welcom back Mrs/Mr ${req.myUser.name} `);
+});
 
-
-
-app.get("/",middelware.isautancate,(req,res)=>
-{
-//users home page
-res.send(`welcom back Mrs/Mr ${req.myUser.name} `)
-
-})
-
-
-
-app.use(errormiddelware)
+app.use(errormiddelware);
